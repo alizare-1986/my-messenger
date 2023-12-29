84b9247e005617ce6067a81dd878b587d03c800a
alizare-1986 <alianayatzare.a.a.1986@gmail.com>
2023-12-29 13:42:57 +0330

    update project messenger

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth/next";
 import authOptions from "./options";
 
-const authOption= authOptions
 
-const handler = NextAuth(authOption);
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
